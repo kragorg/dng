94e4b70a47bc1000a1b427acd68d064478e47c09
Kragor Grimstride <kragor@grimstride.org>
2025-03-23 15:07:27 -0700

    build both full background and print bg by default

--- build.zsh
+++ build.zsh
@@ -1,18 +1,20 @@
 #! /usr/bin/env zsh
 setopt errexit pipefail
 : ${src:=${PWD}} ${out:=outputs/out}
-: ${background:=full}
 
 # Dependencies.
-path[1,0]=( ${tex}/bin ${pandoc}/bin ${coreutils}/bin )
+path[1,0]=( ${tex}/bin ${pandoc}/bin ${gnused}/bin ${coreutils}/bin )
 
 # We need a writable home directory for luaotfload’s font cache.
 export HOME="$PWD"
 
 # Base names for our output files. Additional names will be derived
 # from these.
-outtex='campaign.tex'
-outpdf='Dungeons & Gardens.pdf'
+texbase='campaign.tex'
+pdfbase='Dungeons & Gardens.pdf'
+texprint=${texbase:r}-print.tex
+pdfprint="${pdfbase:r} (Print).pdf"
+pdfbooklet="${pdfbase:r} (Booklet).pdf"
 
 # `markdown` contains the names of the source files, each of which may
 # be shell-quoted.
@@ -27,20 +29,14 @@ cmd=(
   --top-level-division=chapter
   --include-in-header=${src}/include.tex
 )
-pandoc_json=(
-  ${cmd}
-  --to=json
-  --output=${outtex:r}.json
-  ${inputs}
-)
 pandoc_latex=(
   ${cmd}
   --to=latex
   -V documentclass=dndbook
-  -V classoption="12pt,twoside,twocolumn,openany,nodeprecatedcode"
-  -V classoption="letterpaper,bg=${background}"
+  -V classoption="letterpaper,12pt,twoside,twocolumn,openany"
+  -V classoption="nodeprecatedcode,bg=full"
   --lua-filter=${src}/dnd.lua
-  --output=${outtex}
+  --output=${texbase}
   ${inputs}
 )
 fonts=(
@@ -53,8 +49,6 @@ latex=(
   --interaction=batchmode
   --halt-on-error
   --output-format=pdf
-  --jobname=${outpdf:r}
-  ${outtex}
 )
 booklet=(
   pdfjam
@@ -64,49 +58,59 @@ booklet=(
   --landscape
   --longedge
   --booklet true
-  --outfile
-  "${outpdf:r} (Booklet).pdf"
-  ${outpdf}
+  --outfile ${pdfbooklet}
+  ${pdfprint}
 )
 install=(
   install
   -D -t ${out}
   -m 0644
   ${src}/index.html
-  ${outtex}
-  ${outtex:r}.json
-  ${outpdf}
-  "${outpdf:r} (Booklet).pdf"
+  ${texbase}
+  ${pdfbase}
+  ${pdfprint}
+  ${pdfbooklet}
 )
 
 function run {
-  # P: Evaluate parameter name further.
-  # @: Expand into separate words within quotes.
+  # q: Quote arguments.
+  # @: Expand into separate words.
   print -u2
-  print -Pru2 -- "%F{cyan}${(@Pq)${argv[1]}}%f"
-  ${(@P)${argv[1]}} || {
+  print -Pru2 -- "%F{cyan}${(q@)argv}%f"
+  ${argv} || {
     print -Pru2 -- "%F{red}${argv[1]} failed with exit status ${rc::=$?}%f"
     return ${rc}
   }
   print -Pru2 -- '%F{green}'$'\u2714'" ${argv[1]}%f"
 }
 
-print -Pru2 -- "%F{magenta}Building ${outpdf:r}%f"
-run pandoc_json
-run pandoc_latex
-run fonts
+function latex {
+  local cur=''
+  local prev='unset'
+  local rc=0
+  local jobname=$1
+  local input=$2
 
-# Re-run to resolve cross-references, at most five times
-prev='unset'
-repeat 5 {
-  run latex || {
-    rc=$?
-    cat ${outpdf:r}.log
-    exit ${rc}
+  # Re-run to resolve cross-references, at most five times
+  prev='unset'
+  repeat 5 {
+    run ${latex} --jobname=${jobname} ${input} || {
+      rc=$?
+      cat ${pdfbase:r}.log
+      exit ${rc}
+    }
+    [[ ${cur::="$(cksum ${texbase:r}.aux 2>&- || true)"} == ${prev} ]] && break
+    prev=${cur}
   }
-  [[ ${cur::="$(cksum ${outtex:r}.aux 2>&- || true)"} == ${prev} ]] && break
-  prev=${cur}
 }
 
-run booklet
-run install
+print -Pru2 -- "%F{magenta}Building ${pdfbase:r}%f"
+run ${pandoc_latex}
+run sed -E -e 's/,bg=full\]/,bg=print]/' ${texbase} > ${texprint}
+
+run ${fonts}
+latex ${pdfbase:r} ${texbase}
+latex ${pdfprint:r} ${texprint}
+
+run ${booklet}
+run ${install}
